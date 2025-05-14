afed3d89e955728c00a0e8d37aa1380ba79550f6
Oxraphc <caesarravsanjani170@gmail.com>
2025-05-14 12:45:50 +0700

    Declare function playRound

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-function getComputerChoice() {
-    selectionIndex = Math.floor(Math.random() * 10)
-    if (selectionIndex <= 3) {
-        return "Rock"
-    } else if (selectionIndex <= 6) {
-        return "Paper"
+let humanSelection = getHumanChoice().toLowerCase();
+let computerSelection = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats rock!");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper!");
+        computerScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats scissors!");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock!");
+        computerScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper!");
+        computerScore++;
     } else {
-        return "Scissors"
+        console.log("It\'s a tie!")
     }
 }
 
-console.log(getComputerChoice())
+function getComputerChoice() {
+    selectionIndex = Math.floor(Math.random() * 10);
+    if (selectionIndex <= 3) {
+        return "rock"
+    } else if (selectionIndex <= 6) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    return prompt("Enter your move.");
+}
